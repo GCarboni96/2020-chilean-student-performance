12e4c0e8ef6e60d968b26cc656da936dead016e3
Danilo <danilo.moreira@protonmail.com>
2020-07-07 13:37:58 -0400

    some updates

--- src/main/scala/com/education/PerformanceStats.scala
+++ src/main/scala/com/education/PerformanceStats.scala
@@ -1,8 +1,7 @@
 package com.education
 
-import org.apache.spark.sql.functions.{coalesce, col, count, lit, sum, when}
-import org.apache.spark.sql.types.StringType
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.{col, count, lit}
+import org.apache.spark.sql.SparkSession
 
 case class PerformanceStats(){
   def run(spark:SparkSession): Unit ={
@@ -15,6 +14,6 @@ case class PerformanceStats(){
     val total_year = year_agg1.groupBy().sum("count").first.get(0)
     print(total_year)
     val year_agg2 = year_agg1.select(col("agno"), col("count")/lit(total_year))
-    year_agg2.show()
+    year_agg2.coalesce(1).write.option("header", "true").option("delimiter",";").csv("/stats/year")
   }
 }
