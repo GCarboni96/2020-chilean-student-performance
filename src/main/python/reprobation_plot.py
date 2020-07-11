26645d3c8f94c427a3002d6ade5365d170ef3486
Danilo <danilo.moreira@protonmail.com>
2020-07-11 09:18:23 -0400

    some updates

--- src/main/python/reprobation_plot.py
+++ src/main/python/reprobation_plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import glob
 import seaborn as sns
-import os
+
 sns.set(style='ticks')
 
 depe_path = glob.glob("../../../out/reprobation/depe/*.csv")[0]
