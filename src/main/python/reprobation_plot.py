0593ab096de3a67b29a118ea78f0a47c141acf16
Danilo <danilo.moreira@protonmail.com>
2020-07-13 15:01:37 -0400

    stats evaluation

--- src/main/python/reprobation_plot.py
+++ src/main/python/reprobation_plot.py
@@ -13,7 +13,7 @@ rural_path = glob.glob("../../../out/reprobation/rural/*.csv")[0]
 df = pd.read_csv(depe_path, sep=';')
 df['cod_depe'] = df['cod_depe'].astype('category')
 
-color = ["#5C5CC4", "#E7478D", "#90C4FE", "#1BBC9D", "#FFDB4C"]
+color = ["#5C5CC4", "#E7478D", "#90C4FE", "#1BBC9D", "#FFBB4C"]
 
 fig = plt.figure(figsize=(10, 7))
 fig.set_facecolor('#F5FBFB')
@@ -24,6 +24,10 @@ ax.yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
 
 handles, labels = ax.get_legend_handles_labels()
 L = ax.legend(handles=handles[1:], labels=labels[1:])
+
+plt.axvline(2011, color='gray', linestyle='--')
+plt.text(2012.8, 0.005, 'Movilizaciones del 2011',
+         ha='center', va='center',rotation='horizontal', color='gray')
 
 for line in L.get_lines():
     line.set_linewidth(3.0)
@@ -63,6 +67,10 @@ for line in L.get_lines():
 legends = ["Urbano", "Rural"]
 for i in range(2):
     L.get_texts()[i].set_text(legends[i])
+
+plt.axvline(2011, color='gray', linestyle='--')
+plt.text(2012.8, 0.0246, 'Movilizaciones del 2011',
+         ha='center', va='center',rotation='horizontal', color='gray')
 
 plt.xticks(range(2002, 2020, 2))
 plt.xlim([2002, 2019])
--- src/main/python/statistics.py
+++ src/main/python/statistics.py
@@ -6,18 +6,30 @@ import seaborn as sns
 import os
 sns.set(style='ticks')
 
-path = glob.glob("../../../out/stats/*.csv")[0]
+fig = plt.figure(figsize=(10, 7))
+fig.set_facecolor('#F5FBFB')
+
+path = glob.glob("../../../out/stats/performance/*.csv")[0]
 df = pd.read_csv(path, sep=';')
+
+ax = sns.barplot(x="agno", y="proportion", data=df, palette=["#90C4FE"])
+ax.set_facecolor('#F5FBFB')
+plt.xlabel("Año")
+plt.ylabel("Distribución")
+plt.title("Distribución de registros por año Rendimiento Académico")
+ax.yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
 
 fig = plt.figure(figsize=(10, 7))
 fig.set_facecolor('#F5FBFB')
 
-ax = sns.barplot(x="agno",y="proportion", data=df, palette=["#90C4FE"])
+path = glob.glob("../../../out/stats/evaluation/*.csv")[0]
+df = pd.read_csv(path, sep=';')
+
+ax = sns.barplot(x="año_eval", y="proportion", data=df, palette=["#90C4FE"])
 ax.set_facecolor('#F5FBFB')
 plt.xlabel("Año")
 plt.ylabel("Distribución")
+plt.title("Distribución de registros por año Evaluación Docente")
 ax.yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
-
-
 
 plt.show()
